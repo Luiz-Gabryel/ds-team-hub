40ba60dc19099de9e82d47a06fae77402d2f2208
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-04-29 17:42:49 -0300

    att

---
+++ ds-team/src/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', () => {
+  const texto = "Bem-Vindo ao meu Grupo - 2ºDS";
+  const elemento = document.getElementById("texto");
+
+  let i = 0;
+
+  function digitar() {
+    if (i < texto.length) {
+      elemento.textContent += texto[i];
+      i++;
+      setTimeout(digitar, 100);
+    } else {
+      setTimeout(() => {
+        document.getElementById('loading').style.display = 'none';
+      }, 800);
+    }
+  }
+
+  digitar();
+});
