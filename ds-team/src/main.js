4c584e927d348678142f2e8a0ef6d16e8061b511
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-05-08 17:45:08 -0300

    organização por arquivo

--- ds-team/src/main.js
+++ ds-team/src/main.js
@@ -1,3 +1,12 @@
+import "./css/base.css";
+import "./css/navbar.css";
+import "./css/loading.css";
+import "./css/faq.css";
+import "./css/membros.css";
+import "./css/eventos.css";
+import "./css/contato.css";
+import "./css/footer.css";
+
 import { initFaq } from "./faq.js";
 import { initLoading } from "./loading.js";
 import { initMembros } from "./membros.js";
